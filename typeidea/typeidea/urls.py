7e0d2f64e11869a80065d30e79560c70db6d5a33
hongboyang <245035255@qq.com>
2020-04-06 12:55:15 +0800

    改为基于类的view-base

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -13,21 +13,39 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# from django.urls import re_path, path
+# # from django.conf.urls import url
+# from django.contrib import admin
+# from blog.views import post_list, post_detail, login
+# from config.views import links
+# from typeidea.custom_site import custom_site
+#
+#
+# urlpatterns = [
+#     path("login/", login, name='login'),
+#     re_path(r'^$', post_list, name='index'),
+#     re_path(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
+#     re_path(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
+#     re_path(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+#     re_path(r'^links/$', links, name='links'),
+#     re_path(r'^admin/', custom_site.urls, name='super-admin'),
+#     re_path(r'^super_admin/', admin.site.urls, name='admin'),
+# ]
+
 from django.urls import re_path, path
 # from django.conf.urls import url
 from django.contrib import admin
-from blog.views import post_list, post_detail, login
+from blog.views import IndexView, CategoryView, TagView, PostDetailView, login
 from config.views import links
-from typeidea.custom_site import custom_site
-
+from .custom_site import custom_site
 
 urlpatterns = [
-    path("login/", login, name='login'),
-    re_path(r'^$', post_list, name='index'),
-    re_path(r'^category/(?P<category_id>\d+)/$', post_list, name='category-list'),
-    re_path(r'^tag/(?P<tag_id>\d+)/$', post_list, name='tag-list'),
-    re_path(r'^post/(?P<post_id>\d+).html$', post_detail, name='post-detail'),
+    re_path(r"^login/", login, name='login'),
+    re_path(r'^$', IndexView.as_view(), name='index'),
+    re_path(r'^category/(?P<category_id>\d+)/$', CategoryView.as_view(), name='category-list'),
+    re_path(r'^tag/(?P<tag_id>\d+)/$', TagView.as_view(), name='tag-list'),
+    re_path(r'^post/(?P<post_id>\d+).html$', PostDetailView.as_view(), name='post-detail'),
     re_path(r'^links/$', links, name='links'),
-    re_path(r'^admin/', custom_site.urls, name='super-admin'),
-    re_path(r'^super_admin/', admin.site.urls, name='admin'),
+    re_path(r'^super_admin/', admin.site.urls, name='super-admin'),
+    re_path(r'^admin/', custom_site.urls, name='admin'),
 ]
